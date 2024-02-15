69301b1023a3ec8528c062a363055e0896cd78b3
Reid Harrison <reid.al.harrisson@gmail.com>
2024-02-15 16:29:10 +0900

    feat: add rate to review model

--- models/product_reviews.go
+++ models/product_reviews.go
@@ -5,10 +5,11 @@ import "github.com/jinzhu/gorm"
 type ProductReviews struct {
 	gorm.Model
 
-	ProductID  uint64 `gorm:"column:product_id; type:bigint(20) unsigned"`
-	CustomerID uint64 `gorm:"column:customer_id; type:bigint(20) unsigned"`
-	Comment    string `gorm:"column:comment; type:varchar(500)"`
-	Status     string `gorm:"column:status; type:varchar(20)"`
+	ProductID  uint64  `gorm:"column:product_id; type:bigint(20) unsigned"`
+	CustomerID uint64  `gorm:"column:customer_id; type:bigint(20) unsigned"`
+	Comment    string  `gorm:"column:comment; type:text"`
+	Rate       float64 `gorm:"column:rate; type:decimal(20,6)"`
+	Status     string  `gorm:"column:status; type:varchar(20)"`
 }
 
 func (ProductReviews) TableName() string {
