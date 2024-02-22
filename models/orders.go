28c050513fd553c29200f5d106905c40ba2bcf64
Reid Harrison <reid.al.harrisson@gmail.com>
2024-02-23 00:35:36 +0900

    fix: update order and order_items model

--- models/orders.go
+++ models/orders.go
@@ -43,27 +43,27 @@ func OrderStatusToString(orderStatus OrderStatuses) string {
 type Orders struct {
 	gorm.Model
 
-	CustomerID      uint64        `gorm:"column:customer_id; type:bigint(20) unsigned"`
-	Status          OrderStatuses `gorm:"column:status; type:tinyint(4)"`
-	BillingAddress  string        `gorm:"column:billing_address; type:varchar(100)"`
-	ShippingAddress string        `gorm:"column:shipping_address; type:varchar(100)"`
+	CustomerID        uint64        `gorm:"column:customer_id; type:bigint(20) unsigned"`
+	BillingAddressID  string        `gorm:"column:billing_address_id; type:bigint(20) unsigned"`
+	ShippingAddressID string        `gorm:"column:shipping_address_id; type:bigint(20) unsigned"`
+	Status            OrderStatuses `gorm:"column:status; type:tinyint(4)"`
 }
 
 type OrderItems struct {
 	gorm.Model
 
-	OrderID        uint64        `gorm:"column:order_id; type:bigint(20) unsigned"`
-	StoreID        uint64        `gorm:"column:store_id; type:bigint(20) unsigned"`
-	ProductID      uint64        `gorm:"column:product_id; type:bigint(20) unsigned"`
-	UnitPriceSale  float64       `gorm:"column:unit_price_sale; type:decimal(20,6)"`
-	Quantity       float64       `gorm:"column:quantity; type:decimal(20,6)"`
-	SubTotalPrice  float64       `gorm:"column:sub_total_price; type:decimal(20,6)"`
-	TaxRate        float64       `gorm:"column:tax_rate; type:decimal(20,6)"`
-	TaxAmount      float64       `gorm:"column:tax_amount; type:decimal(20,6)"`
-	ShippingMethod string        `gorm:"column:shipping_method; type:varchar(20)"`
-	ShippingPrice  float64       `gorm:"column:shipping_price; type:deciaml(20,6)"`
-	TotalPrice     float64       `gorm:"column:total_price; type:decimal(20,6)"`
-	Status         OrderStatuses `gorm:"column:status; type:tinyint(4)"`
+	OrderID          uint64        `gorm:"column:order_id; type:bigint(20) unsigned"`
+	StoreID          uint64        `gorm:"column:store_id; type:bigint(20) unsigned"`
+	VariationID      uint64        `gorm:"column:variation_id; type:bigint(20) unsigned"`
+	Price            float64       `gorm:"column:price; type:decimal(20,6)"`
+	Quantity         float64       `gorm:"column:quantity; type:decimal(20,6)"`
+	SubTotalPrice    float64       `gorm:"column:sub_total_price; type:decimal(20,6)"`
+	TaxRate          float64       `gorm:"column:tax_rate; type:decimal(20,6)"`
+	TaxAmount        float64       `gorm:"column:tax_amount; type:decimal(20,6)"`
+	ShippingMethodID string        `gorm:"column:shipping_method_id; type:bigint(20) unsigned"`
+	ShippingPrice    float64       `gorm:"column:shipping_price; type:deciaml(20,6)"`
+	TotalPrice       float64       `gorm:"column:total_price; type:decimal(20,6)"`
+	Status           OrderStatuses `gorm:"column:status; type:tinyint(4)"`
 }
 
 type CustomerOrders struct {
