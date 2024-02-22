78852fb715d984c57b74b79ca384c317e6ca5ed5
Reid Harrison <reid.al.harrisson@gmail.com>
2024-02-22 23:57:52 +0900

    fix: update categories

--- services/categories/create.go
+++ services/categories/create.go
@@ -5,7 +5,7 @@ import (
 	"OnlineStoreBackend/requests"
 )
 
-func (service *Service) Create(modelCategory *models.BaseCategories, req *requests.RequestCategory, storeID uint64) {
+func (service *Service) Create(modelCategory *models.StoreCategories, req *requests.RequestCategory, storeID uint64) {
 	modelCategory.Name = req.Name
 	modelCategory.StoreID = storeID
 	modelCategory.ParentID = req.ParentID
