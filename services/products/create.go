e26d16ced156c65c70ce99f3455eeb295a00366c
Reid Harrison <reid.al.harrisson@gmail.com>
2024-03-01 07:28:44 +0900

    fix: resolve duplicate errors

--- services/products/create.go
+++ services/products/create.go
@@ -104,55 +104,60 @@ func (service *Service) CreateWithCSV(modelProduct *models.Products, modelCsv mo
 
 	switch modelCsv.Type {
 	case "simple":
-		imageUrls := strings.Split(modelCsv.Images, ",")
-		images, _ := json.Marshal(imageUrls)
-		modelProduct.StoreID = storeID
-		modelProduct.Title = modelCsv.Name
-		modelProduct.ShortDescription = modelCsv.ShortDescription
-		modelProduct.LongDescription = modelCsv.Description
-		modelProduct.ImageUrls = string(images)
-		modelProduct.MinimumStockLevel, _ = strconv.ParseFloat(modelCsv.LowStockAmount, 64)
-		switch modelCsv.Published {
-		case "1":
-			modelProduct.Status = utils.Approved
-		case "0":
-			modelProduct.Status = utils.Draft
-		}
-		modelProduct.ShippingClass = modelCsv.ShippingClass
-		modelProduct.Sku = modelCsv.Sku
-		modelProduct.Type = utils.Simple
-		service.DB.Create(modelProduct)
+		service.DB.Where("sku = ?", modelCsv.Sku).First(&modelProduct)
+		if modelProduct.ID == 0 {
+			imageUrls := strings.Split(modelCsv.Images, ",")
+			images, _ := json.Marshal(imageUrls)
+			modelProduct.StoreID = storeID
+			modelProduct.Title = modelCsv.Name
+			modelProduct.ShortDescription = modelCsv.ShortDescription
+			modelProduct.LongDescription = modelCsv.Description
+			modelProduct.ImageUrls = string(images)
+			modelProduct.MinimumStockLevel, _ = strconv.ParseFloat(modelCsv.LowStockAmount, 64)
+			switch modelCsv.Published {
+			case "1":
+				modelProduct.Status = utils.Approved
+			case "0":
+				modelProduct.Status = utils.Draft
+			}
+			modelProduct.ShippingClass = modelCsv.ShippingClass
+			modelProduct.Sku = modelCsv.Sku
+			modelProduct.Type = utils.Simple
+			service.DB.Create(modelProduct)
 
-		productID := uint64(modelProduct.ID)
-		prodcateService.CreateWithCSV(modelCategories, productID)
-		prodtagService.CreateWithCSV(modelTags, productID)
-		modelVar := models.ProductVariations{}
-		varService.CreateSimpleWithCSV(&modelVar, &modelCsv, productID)
+			productID := uint64(modelProduct.ID)
+			prodcateService.CreateWithCSV(modelCategories, productID)
+			prodtagService.CreateWithCSV(modelTags, productID)
+			modelVar := models.ProductVariations{}
+			varService.CreateSimpleWithCSV(&modelVar, &modelCsv, productID)
+		}
 	case "variable":
-		imageUrls := strings.Split(modelCsv.Images, ",")
-		images, _ := json.Marshal(imageUrls)
-		modelProduct.StoreID = storeID
-		modelProduct.Title = modelCsv.Name
-		modelProduct.ShortDescription = modelCsv.ShortDescription
-		modelProduct.LongDescription = modelCsv.Description
-		modelProduct.ImageUrls = string(images)
-		modelProduct.MinimumStockLevel, _ = strconv.ParseFloat(modelCsv.LowStockAmount, 64)
-		switch modelCsv.Published {
-		case "1":
-			modelProduct.Status = utils.Approved
-		case "0":
-			modelProduct.Status = utils.Draft
+		service.DB.Where("sku = ?", modelCsv.Sku).First(&modelProduct)
+		if modelProduct.ID == 0 {
+			imageUrls := strings.Split(modelCsv.Images, ",")
+			images, _ := json.Marshal(imageUrls)
+			modelProduct.StoreID = storeID
+			modelProduct.Title = modelCsv.Name
+			modelProduct.ShortDescription = modelCsv.ShortDescription
+			modelProduct.LongDescription = modelCsv.Description
+			modelProduct.ImageUrls = string(images)
+			modelProduct.MinimumStockLevel, _ = strconv.ParseFloat(modelCsv.LowStockAmount, 64)
+			switch modelCsv.Published {
+			case "1":
+				modelProduct.Status = utils.Approved
+			case "0":
+				modelProduct.Status = utils.Draft
+			}
+			modelProduct.Sku = modelCsv.Sku
+			modelProduct.ShippingClass = modelCsv.ShippingClass
+			modelProduct.Type = utils.Variable
+			service.DB.Create(modelProduct)
+
+			productID := uint64(modelProduct.ID)
+			prodcateService.CreateWithCSV(modelCategories, productID)
+			prodtagService.CreateWithCSV(modelTags, productID)
+			attrService.CreateWithCSV(&modelCsv, productID)
 		}
-		modelProduct.Sku = modelCsv.Sku
-		modelProduct.ShippingClass = modelCsv.ShippingClass
-		modelProduct.Type = utils.Variable
-		service.DB.Create(modelProduct)
-
-		productID := uint64(modelProduct.ID)
-		prodcateService.CreateWithCSV(modelCategories, productID)
-		prodtagService.CreateWithCSV(modelTags, productID)
-		attrService.CreateWithCSV(&modelCsv, productID)
-
 	case "variation":
 		service.DB.Where("sku = ?", modelCsv.Parent).First(&modelProduct)
 		modelVals := make([]models.ProductAttributeValues, 0)
@@ -182,6 +187,9 @@ func (service *Service) CreateWithCSV(modelProduct *models.Products, modelCsv mo
 		}
 		attrService.UpdateWithCSV(&modelVals, &modelCsv, uint64(modelProduct.ID))
 		modelVar := models.ProductVariations{}
-		varService.CreateVariableWithCSV(&modelVar, &modelCsv, uint64(modelProduct.ID), &modelVals)
+		varService.DB.Where("sku = ?", modelCsv.Sku).First(&modelVar)
+		if modelVar.ID == 0 {
+			varService.CreateVariableWithCSV(&modelVar, &modelCsv, uint64(modelProduct.ID), &modelVals)
+		}
 	}
 }
