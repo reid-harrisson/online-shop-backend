b4c5e09ae78d539741454cf092ae55baf34500bb
Reid Harrison <reid.al.harrisson@gmail.com>
2024-03-05 15:54:54 +0900

    feat: add health check handler

---
+++ server/handlers/health.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"OnlineStoreBackend/responses"
+	s "OnlineStoreBackend/server"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type HealthHandler struct {
+	server *s.Server
+}
+
+func NewHealthHandler(server *s.Server) *HealthHandler {
+	return &HealthHandler{server: server}
+}
+
+// HealthCheck godoc
+// @Summary Health Check
+// @Description Check server's health
+// @ID health-check
+// @Tags Health Check Actions
+// @Accept json
+// @Produce json
+// @Success 200 {object} responses.Data
+// @Router /store/health [get]
+func (healthHandler *HealthHandler) HealthCheck(c echo.Context) error {
+	return responses.MessageResponse(c, http.StatusOK, "Server is running!")
+}
