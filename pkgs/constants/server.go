f18454c11c01b3acc4a3ffadffd12d18da69aa96
Reid Harrison <reid.al.harrisson@gmail.com>
2024-04-10 14:08:53 +0200

    feat: add error handler

---
+++ pkgs/constants/server.go
@@ -0,0 +1,7 @@
+package constants
+
+var (
+	InternalServerErrorMessage = "Something's wrong with the Server"
+	InvalidData                = "Invalid Data"
+	NotFound                   = "No Data"
+)
---
+++ pkgs/error/error_handle.go
@@ -0,0 +1,39 @@
+package errhandle
+
+import (
+	"OnlineStoreBackend/pkgs/constants"
+	"net/http"
+
+	"github.com/go-sql-driver/mysql"
+	"gorm.io/gorm"
+)
+
+func SqlErrorHandler(err error) (int, string) {
+	sqlErr, ok := err.(*mysql.MySQLError)
+	if ok {
+		switch sqlErr.Number {
+		case constants.ErrInvalidData:
+			return http.StatusBadRequest, constants.InvalidData
+		case constants.ErrInvalidValueOfLength:
+			return http.StatusBadRequest, constants.InvalidData
+		case constants.ErrDuplicatedKeyRequired:
+			return http.StatusBadRequest, constants.InvalidData
+		case constants.ErrForeignKeyViolated:
+			return http.StatusBadRequest, constants.InvalidData
+		case constants.ErrInvalidField:
+			return http.StatusInternalServerError, constants.InternalServerErrorMessage
+		case constants.ErrRecordNotFound:
+			return http.StatusNotFound, constants.NotFound
+		case constants.ErrMissingWhereClause:
+			return http.StatusInternalServerError, constants.InternalServerErrorMessage
+		}
+	} else {
+		if err == gorm.ErrEmptySlice {
+			return http.StatusInternalServerError, constants.InternalServerErrorMessage
+		}
+	}
+	if err != nil {
+		return http.StatusInternalServerError, constants.InternalServerErrorMessage
+	}
+	return 0, ""
+}
