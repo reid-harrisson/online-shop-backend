0c5422a808fe0439daca77018b988047cef1b4dc
Reid Harrison <reid.al.harrisson@gmail.com>
2024-04-27 03:47:12 +0900

    fix: update coupon repository

--- repositories/coupons.go
+++ repositories/coupons.go
@@ -15,17 +15,28 @@ func NewRepositoryCoupon(db *gorm.DB) *RepositoryCoupon {
 }
 
 func (repository *RepositoryCoupon) ReadByStoreID(modelCoupons *[]models.Coupons, storeID uint64) error {
-	return repository.DB.Where("store_id = ?", storeID).Find(modelCoupons).Error
+	return repository.DB.
+		Where("store_id = ?", storeID).
+		Find(modelCoupons).
+		Error
 }
 
 func (repository *RepositoryCoupon) ReadByID(modelCoupon *models.Coupons, couponID uint64) error {
-	return repository.DB.First(modelCoupon, couponID).Error
+	return repository.DB.
+		First(modelCoupon, couponID).
+		Error
 }
 
 func (repository *RepositoryCoupon) ReadByCode(modelCoupon *models.Coupons, code string) error {
-	return repository.DB.Where("coupon_code = ?", code).First(modelCoupon).Error
+	return repository.DB.
+		Where("coupon_code = ?", code).
+		First(modelCoupon).
+		Error
 }
 
 func (repository *RepositoryCoupon) ReadByIDs(modelCoupon *[]models.Coupons, ids []uint64) error {
-	return repository.DB.Where("id In (?)", ids).Find(modelCoupon).Error
+	return repository.DB.
+		Where("id In (?)", ids).
+		Find(modelCoupon).
+		Error
 }
