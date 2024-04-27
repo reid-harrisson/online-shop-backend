2a3a24cca5b6885452ae979bf79a2eaaf1f7eb3e
Reid Harrison <reid.al.harrisson@gmail.com>
2024-04-28 01:27:42 +0900

    feat: add read by id unit test

--- test/repositories/combo_test.go
+++ test/repositories/combo_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 var (
-	comboOutputs = []models.ComboItems{
+	comboItemsOutputs = []models.ComboItems{
 		{
 			ComboID:     1,
 			VariationID: 1,
@@ -20,6 +20,17 @@ var (
 			ComboID:     2,
 			VariationID: 2,
 			Quantity:    2,
+		},
+	}
+	comboOutputs = []models.Combos{
+		{
+			StoreID:        1,
+			DiscountAmount: 10.000,
+			DiscountType:   1,
+			ImageUrls:      "https://www.chegourmet.co.za/wp-content/uploads/2019/09/Gochujang-Front-scaled.jpg",
+			Description:    "Combo of Kefir",
+			Title:          "Kefir Combo",
+			Status:         2,
 		},
 	}
 )
@@ -42,9 +53,9 @@ func TestComboReadByStoreID(t *testing.T) {
 
 	// Assertions
 	if assert.NoError(t, comboRepo.ReadByStoreID(&modelCombos, &modelComboItems, 1)) {
-		assert.Equal(t, comboOutputs[0].ComboID, modelComboItems[0].ComboID)
-		assert.Equal(t, comboOutputs[0].VariationID, modelComboItems[0].VariationID)
-		assert.Equal(t, comboOutputs[0].ComboID, modelComboItems[0].ComboID)
+		assert.Equal(t, comboItemsOutputs[0].ComboID, modelComboItems[0].ComboID)
+		assert.Equal(t, comboItemsOutputs[0].VariationID, modelComboItems[0].VariationID)
+		assert.Equal(t, comboItemsOutputs[0].ComboID, modelComboItems[0].ComboID)
 	}
 }
 
@@ -66,9 +77,9 @@ func TestComboReadApproved(t *testing.T) {
 
 	// Assertions
 	if assert.NoError(t, comboRepo.ReadApproved(&modelCombos, &modelComboItems, 1)) {
-		assert.Equal(t, comboOutputs[0].ComboID, modelComboItems[0].ComboID)
-		assert.Equal(t, comboOutputs[0].VariationID, modelComboItems[0].VariationID)
-		assert.Equal(t, comboOutputs[0].ComboID, modelComboItems[0].ComboID)
+		assert.Equal(t, comboItemsOutputs[0].ComboID, modelComboItems[0].ComboID)
+		assert.Equal(t, comboItemsOutputs[0].VariationID, modelComboItems[0].VariationID)
+		assert.Equal(t, comboItemsOutputs[0].ComboID, modelComboItems[0].ComboID)
 	}
 }
 
@@ -90,8 +101,35 @@ func TestComboReadPublished(t *testing.T) {
 
 	// Assertions
 	if assert.NoError(t, comboRepo.ReadPublished(&modelCombos, &modelComboItems, 2)) {
-		assert.Equal(t, comboOutputs[1].ComboID, modelComboItems[0].ComboID)
-		assert.Equal(t, comboOutputs[1].VariationID, modelComboItems[0].VariationID)
-		assert.Equal(t, comboOutputs[1].ComboID, modelComboItems[0].ComboID)
+		assert.Equal(t, comboItemsOutputs[1].ComboID, modelComboItems[0].ComboID)
+		assert.Equal(t, comboItemsOutputs[1].VariationID, modelComboItems[0].VariationID)
+		assert.Equal(t, comboItemsOutputs[1].ComboID, modelComboItems[0].ComboID)
+	}
+}
+
+func TestComboReadByID(t *testing.T) {
+	cfg := test_utils.PrepareAllConfiguration("./../../config.test.yaml")
+
+	// DB Connection
+	db := test_utils.InitTestDB(cfg)
+	test_utils.ResetCombosDB(db)
+	test_utils.ResetComboItemsDB(db)
+	test_utils.ResetStoresDB(db)
+	test_utils.ResetVariationsDB(db)
+
+	// Setup
+	var comboRepo = repositories.NewRepositoryCombo(db)
+
+	var modelCombos = models.Combos{}
+
+	// Assertions
+	if assert.NoError(t, comboRepo.ReadByID(&modelCombos, 1)) {
+		assert.Equal(t, comboOutputs[0].StoreID, modelCombos.StoreID)
+		assert.Equal(t, comboOutputs[0].DiscountAmount, modelCombos.DiscountAmount)
+		assert.Equal(t, comboOutputs[0].DiscountType, modelCombos.DiscountType)
+		assert.Equal(t, comboOutputs[0].ImageUrls, modelCombos.ImageUrls)
+		assert.Equal(t, comboOutputs[0].Description, modelCombos.Description)
+		assert.Equal(t, comboOutputs[0].Title, modelCombos.Title)
+		assert.Equal(t, comboOutputs[0].Status, modelCombos.Status)
 	}
 }
