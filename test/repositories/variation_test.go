d5db1c81bfc8671f9bcd9c4e44a2f2e3e18a4610
Reid Harrison <reid.al.harrisson@gmail.com>
2024-04-30 04:17:20 +0900

    feat: add read variation by id unit test

--- test/repositories/variation_test.go
+++ test/repositories/variation_test.go
@@ -20,6 +20,20 @@ var (
 			Unit:             "",
 		},
 	}
+	readVariationsOutputs = []models.Variations{
+		{
+			ProductID:       1,
+			Sku:             "44-125G",
+			Price:           96.00,
+			StockLevel:      10.00,
+			DiscountAmount:  20.00,
+			DiscountType:    1,
+			ImageUrls:       "",
+			Description:     "Full cream milk kefir made using live kefir grains/cultures, fermented traditionally for maximum probiotic diversity.",
+			Title:           "Gochujang - Korean Chilli Pepper Paste - 125G",
+			BackOrderStatus: 0,
+		},
+	}
 )
 
 func TestReadVariationsByProduct(t *testing.T) {
@@ -47,5 +61,31 @@ func TestReadVariationsByProduct(t *testing.T) {
 				assert.Equal(t, readVarsWithAttrValue[index], modelVars[index])
 			}
 		}
+	}
+}
+
+func TestReadVariationsByID(t *testing.T) {
+	cfg := test_utils.PrepareAllConfiguration("./../../config.test.yaml")
+
+	// DB Connection
+	db := test_utils.InitTestDB(cfg)
+	test_utils.ResetStoresDB(db)
+	test_utils.ResetAttributeValuesDB(db)
+	test_utils.ResetProductsDB(db)
+	test_utils.ResetVariationsDB(db)
+	test_utils.ResetVariationDetailsDB(db)
+	test_utils.ResetAttributesDB(db)
+
+	// Setup
+	varRepo := repositories.NewRepositoryVariation(db)
+	modelVars := models.Variations{}
+
+	// Assertions
+	if assert.NoError(t, varRepo.ReadByID(&modelVars, 1)) {
+		readVariationsOutputs[0].ID = modelVars.ID
+		readVariationsOutputs[0].CreatedAt = modelVars.CreatedAt
+		readVariationsOutputs[0].UpdatedAt = modelVars.UpdatedAt
+
+		assert.Equal(t, readVariationsOutputs[0], modelVars)
 	}
 }
