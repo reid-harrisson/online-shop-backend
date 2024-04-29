ac9eb91f06f1bafe21b5facc853f663f07e986fa
Reid Harrison <reid.al.harrisson@gmail.com>
2024-04-29 16:59:28 +0900

    feat: add read customer insights analytics unit test

--- test/repositories/analytics_test.go
+++ test/repositories/analytics_test.go
@@ -22,6 +22,16 @@ var (
 			TotalPrice:  0,
 		},
 	}
+	readCustomerInsightsOutputs = []models.CustomerInsights{
+		{
+			MaleCount:   0,
+			FemaleCount: 0,
+			AverageAge:  0,
+			YoungestAge: 0,
+			OldestAge:   0,
+			Location:    "",
+		},
+	}
 )
 
 func TestAnalyticsReadSalesReport(t *testing.T) {
@@ -44,5 +54,31 @@ func TestAnalyticsReadSalesReport(t *testing.T) {
 		if assert.Equal(t, len(readSaleReportOutputs), len(modelReports)) {
 			assert.Equal(t, readSaleReportOutputs[0].StoreID, modelReports[0].StoreID)
 		}
+	}
+}
+
+func TestAnalyticsReadCustomerInsights(t *testing.T) {
+	cfg := test_utils.PrepareAllConfiguration("./../../config.test.yaml")
+
+	// DB Connection
+	db := test_utils.InitTestDB(cfg)
+	test_utils.ResetStoreOrdersDB(db)
+	test_utils.ResetStoreOrderItemsDB(db)
+	test_utils.ResetUsersDB(db)
+
+	// Setup
+	analRepo := repositories.NewRepositoryAnalytics(db)
+	var modelReports = models.CustomerInsights{}
+
+	// Assert
+	var startDate = time.Date(1, 1, 1, 0, 0, 0, 0, time.Local)
+	var endDate = time.Date(2025, 1, 1, 0, 0, 0, 0, time.Local)
+	if assert.NoError(t, analRepo.ReadCustomerInsights(&modelReports, 1, startDate, endDate)) {
+		assert.Equal(t, readCustomerInsightsOutputs[0].MaleCount, modelReports.MaleCount)
+		assert.Equal(t, readCustomerInsightsOutputs[0].FemaleCount, modelReports.FemaleCount)
+		assert.Equal(t, readCustomerInsightsOutputs[0].AverageAge, modelReports.AverageAge)
+		assert.Equal(t, readCustomerInsightsOutputs[0].YoungestAge, modelReports.YoungestAge)
+		assert.Equal(t, readCustomerInsightsOutputs[0].OldestAge, modelReports.OldestAge)
+		assert.Equal(t, readCustomerInsightsOutputs[0].Location, modelReports.Location)
 	}
 }
