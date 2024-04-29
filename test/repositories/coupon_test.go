5567bc5a0542f97d49d5064fbc519e7d56ea13c2
Reid Harrison <reid.al.harrisson@gmail.com>
2024-04-29 12:02:35 +0900

    feat: add read coupon by code unit test

--- test/repositories/coupon_test.go
+++ test/repositories/coupon_test.go
@@ -47,7 +47,7 @@ func TestCouponReadByStoreID(t *testing.T) {
 	}
 }
 
-func TestCouponReadID(t *testing.T) {
+func TestCouponReadByID(t *testing.T) {
 	cfg := test_utils.PrepareAllConfiguration("./../../config.test.yaml")
 
 	// DB Connection
@@ -57,16 +57,41 @@ func TestCouponReadID(t *testing.T) {
 	// Setup
 	var couponRepo = repositories.NewRepositoryCoupon(db)
 
-	var modelCoupon = []models.Coupons{}
+	var modelCoupon = models.Coupons{}
 
 	// Assertions
-	if assert.NoError(t, couponRepo.ReadByStoreID(&modelCoupon, 1)) {
-		assert.Equal(t, couponOutputs[0].StoreID, modelCoupon[0].StoreID)
-		assert.Equal(t, couponOutputs[0].CouponCode, modelCoupon[0].CouponCode)
-		assert.Equal(t, couponOutputs[0].DiscountType, modelCoupon[0].DiscountType)
-		assert.Equal(t, couponOutputs[0].CouponAmount, modelCoupon[0].CouponAmount)
-		assert.Equal(t, couponOutputs[0].AllowFreeShipping, modelCoupon[0].AllowFreeShipping)
-		assert.Equal(t, couponOutputs[0].MinimumSpend, modelCoupon[0].MinimumSpend)
-		assert.Equal(t, couponOutputs[0].MaximumSpend, modelCoupon[0].MaximumSpend)
+	if assert.NoError(t, couponRepo.ReadByID(&modelCoupon, 1)) {
+		assert.Equal(t, couponOutputs[0].StoreID, modelCoupon.StoreID)
+		assert.Equal(t, couponOutputs[0].CouponCode, modelCoupon.CouponCode)
+		assert.Equal(t, couponOutputs[0].DiscountType, modelCoupon.DiscountType)
+		assert.Equal(t, couponOutputs[0].CouponAmount, modelCoupon.CouponAmount)
+		assert.Equal(t, couponOutputs[0].AllowFreeShipping, modelCoupon.AllowFreeShipping)
+		assert.Equal(t, couponOutputs[0].MinimumSpend, modelCoupon.MinimumSpend)
+		assert.Equal(t, couponOutputs[0].MaximumSpend, modelCoupon.MaximumSpend)
+	}
+}
+
+func TestCouponReadByCode(t *testing.T) {
+	cfg := test_utils.PrepareAllConfiguration("./../../config.test.yaml")
+
+	// DB Connection
+	db := test_utils.InitTestDB(cfg)
+	test_utils.ResetCouponsDB(db)
+
+	// Setup
+	var couponRepo = repositories.NewRepositoryCoupon(db)
+
+	var modelCoupon = models.Coupons{}
+
+	// Assertions
+	var code = "30PERCENTMIN"
+	if assert.NoError(t, couponRepo.ReadByCode(&modelCoupon, code)) {
+		assert.Equal(t, couponOutputs[0].StoreID, modelCoupon.StoreID)
+		assert.Equal(t, couponOutputs[0].CouponCode, modelCoupon.CouponCode)
+		assert.Equal(t, couponOutputs[0].DiscountType, modelCoupon.DiscountType)
+		assert.Equal(t, couponOutputs[0].CouponAmount, modelCoupon.CouponAmount)
+		assert.Equal(t, couponOutputs[0].AllowFreeShipping, modelCoupon.AllowFreeShipping)
+		assert.Equal(t, couponOutputs[0].MinimumSpend, modelCoupon.MinimumSpend)
+		assert.Equal(t, couponOutputs[0].MaximumSpend, modelCoupon.MaximumSpend)
 	}
 }
