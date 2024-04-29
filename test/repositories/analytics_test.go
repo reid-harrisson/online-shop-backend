ee828b2caa3e1c26e4e4cb6833beaf2f5195d658
Reid Harrison <reid.al.harrisson@gmail.com>
2024-04-30 03:03:26 +0900

    feat: add read customer date by location unit test

--- test/repositories/analytics_test.go
+++ test/repositories/analytics_test.go
@@ -408,3 +408,20 @@ func TestAnalyticsReadOrderTrend(t *testing.T) {
 	var endDate = time.Date(2025, 1, 1, 0, 0, 0, 0, time.Local)
 	assert.NoError(t, analRepo.ReadOrderTrendAnalytics(&modelReports, 1, startDate, endDate))
 }
+
+func TestAnalyticsReadCustomerDataByLocation(t *testing.T) {
+	cfg := test_utils.PrepareAllConfiguration("./../../config.test.yaml")
+
+	// DB Connection
+	db := test_utils.InitTestDB(cfg)
+	test_utils.ResetVisitorsDB(db)
+
+	// Setup
+	analRepo := repositories.NewRepositoryAnalytics(db)
+	var modelReports = []models.CustomerDataByLocation{}
+
+	// Assert
+	var startDate = time.Date(1, 1, 1, 0, 0, 0, 0, time.Local)
+	var endDate = time.Date(2025, 1, 1, 0, 0, 0, 0, time.Local)
+	assert.NoError(t, analRepo.ReadCustomerDataByLocation(&modelReports, 1, startDate, endDate))
+}
