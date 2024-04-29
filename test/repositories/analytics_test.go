303a95158c9acf096f61aeeb398dc25b1a0d9871
Reid Harrison <reid.al.harrisson@gmail.com>
2024-04-29 17:38:41 +0900

    feat: add read visitor analytics unit test

--- test/repositories/analytics_test.go
+++ test/repositories/analytics_test.go
@@ -38,6 +38,14 @@ var (
 			StockOut: 0,
 		},
 	}
+	readVisitorsOutputs = []models.VisitorAnalytics{
+		{
+			Visitor:       1,
+			UniqueVisitor: 1,
+			PageView:      1,
+			BounceRate:    1,
+		},
+	}
 )
 
 func TestAnalyticsReadSalesReport(t *testing.T) {
@@ -212,5 +220,27 @@ func TestAnalyticsReadStockAnalyticMonth(t *testing.T) {
 	if assert.NoError(t, analRepo.ReadStockAnalyticMonth(&modelReports, 1, startDate, endDate)) {
 		assert.Equal(t, readStockOutputs[0].StockIn, modelReports[0].StockIn)
 		assert.Equal(t, readStockOutputs[0].StockOut, modelReports[0].StockOut)
+	}
+}
+
+func TestAnalyticsReadVisitor(t *testing.T) {
+	cfg := test_utils.PrepareAllConfiguration("./../../config.test.yaml")
+
+	// DB Connection
+	db := test_utils.InitTestDB(cfg)
+	test_utils.ResetVisitorsDB(db)
+
+	// Setup
+	analRepo := repositories.NewRepositoryAnalytics(db)
+	var modelReports = models.VisitorAnalytics{}
+
+	// Assert
+	var startDate = time.Date(1, 1, 1, 0, 0, 0, 0, time.Local)
+	var endDate = time.Date(2025, 1, 1, 0, 0, 0, 0, time.Local)
+	if assert.NoError(t, analRepo.ReadVisitor(&modelReports, 1, startDate, endDate)) {
+		assert.Equal(t, readVisitorsOutputs[0].Visitor, modelReports.Visitor)
+		assert.Equal(t, readVisitorsOutputs[0].UniqueVisitor, modelReports.UniqueVisitor)
+		assert.Equal(t, readVisitorsOutputs[0].PageView, modelReports.PageView)
+		assert.Equal(t, readVisitorsOutputs[0].BounceRate, modelReports.BounceRate)
 	}
 }
