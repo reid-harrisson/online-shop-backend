829b5cb2885d5f85718e3ed2a43e2fa4ab48d043
Reid Harrison <reid.al.harrisson@gmail.com>
2024-04-29 10:17:06 +0900

    feat: add read combo detail unit test

--- test/repositories/combo_test.go
+++ test/repositories/combo_test.go
@@ -33,6 +33,27 @@ var (
 			Status:         2,
 		},
 	}
+	comboItemsWithDetail = []models.CartItemsWithDetail{
+		{
+			CartItems: models.CartItems{
+				CustomerID:  1,
+				VariationID: 1,
+				Quantity:    1,
+			},
+			StoreID:        1,
+			VariationName:  "Gochujang - Korean Chilli Pepper Paste - 125G",
+			ImageUrls:      "",
+			Price:          96.000000,
+			Categories:     "",
+			DiscountAmount: 20.000000,
+			DiscountType:   1,
+			StockLevel:     10.000000,
+			Weight:         1.0,
+			Width:          1.0,
+			Height:         1.0,
+			Length:         1.0,
+		},
+	}
 )
 
 func TestComboReadByStoreID(t *testing.T) {
@@ -116,6 +137,34 @@ func TestComboReadByID(t *testing.T) {
 	test_utils.ResetComboItemsDB(db)
 	test_utils.ResetStoresDB(db)
 	test_utils.ResetVariationsDB(db)
+
+	// Setup
+	var comboRepo = repositories.NewRepositoryCombo(db)
+
+	var modelCombos = models.Combos{}
+
+	// Assertions
+	if assert.NoError(t, comboRepo.ReadByID(&modelCombos, 1)) {
+		assert.Equal(t, comboOutputs[0].StoreID, modelCombos.StoreID)
+		assert.Equal(t, comboOutputs[0].DiscountAmount, modelCombos.DiscountAmount)
+		assert.Equal(t, comboOutputs[0].DiscountType, modelCombos.DiscountType)
+		assert.Equal(t, comboOutputs[0].ImageUrls, modelCombos.ImageUrls)
+		assert.Equal(t, comboOutputs[0].Description, modelCombos.Description)
+		assert.Equal(t, comboOutputs[0].Title, modelCombos.Title)
+		assert.Equal(t, comboOutputs[0].Status, modelCombos.Status)
+	}
+}
+
+func TestComboReadDetail(t *testing.T) {
+	cfg := test_utils.PrepareAllConfiguration("./../../config.test.yaml")
+
+	// DB Connection
+	db := test_utils.InitTestDB(cfg)
+	test_utils.ResetCombosDB(db)
+	test_utils.ResetComboItemsDB(db)
+	test_utils.ResetStoresDB(db)
+	test_utils.ResetVariationsDB(db)
+	test_utils.ResetShippingData(db)
 
 	// Setup
 	var comboRepo = repositories.NewRepositoryCombo(db)
