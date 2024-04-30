af301027969d4ece1e1cbf32190cbb9e35ccfc6e
Reid Harrison <reid.al.harrisson@gmail.com>
2024-05-01 03:37:44 +0900

    fix: fix checking duplicate coupon code

--- pkgs/constants/coupon.go
+++ pkgs/constants/coupon.go
@@ -4,4 +4,5 @@ var (
 	CouponNotFound   = "This coupon doesn't exist."
 	CouponDuplicated = "This coupon code already exist."
 	CouponDeleted    = "Coupon successfully deleted."
+	DuplicateCoupon  = "This coupon code already exist."
 )
